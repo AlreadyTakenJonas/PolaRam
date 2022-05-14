7c8dc63af9d1d069cc5d2e66222c1a7f723b10db
Jonas Eichhorn <jonas@eichhorn-networks.de>
2022-05-14 13:23:46 +0200

    Removed depreceated conda env export shit

--- conda_env_export.sh
+++
@@ -1,4 +0,0 @@
-# Save conda environment to file without including the dependencies. Prevents cross-platform issues when sharing the environment.
-echo "Creating conda_env.yml ..."
-conda env export --prefix ./conda_env --file conda_env.yml
-
